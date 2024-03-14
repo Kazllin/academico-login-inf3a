635098d25ff07e04c1353dbfa0ac6fbced89abc1
Marcos Henrique Oliveira Santos <rm87320@estudante.fieb.edu.br>
2024-03-13 21:10:09 -0300

    Nao terminado

--- src/main/java/com/itb/lip2/academicologininf3a/controller/UsuarioController.java
+++ src/main/java/com/itb/lip2/academicologininf3a/controller/UsuarioController.java
@@ -3,15 +3,27 @@ package com.itb.lip2.academicologininf3a.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itb.lip2.academicologininf3a.model.Usuario;
+import com.itb.lip2.academicologininf3a.repository.UsuarioRepository;
+import com.itb.lip2.academicologininf3a.service.UsuarioService;
+
 
 @RestController
 @RequestMapping("/api/v1")
 public class UsuarioController {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	private UsuarioService usuarioService;
 
 	@GetMapping("/User")
 	public List<Usuario> getUsers() {
@@ -31,6 +43,15 @@ public class UsuarioController {
 		usuarios.add(us1);
 		usuarios.add(us2);
 		return usuarios;
+		
+	}
+	
+	@PostMapping("/User")
+	public ResponseEntity<Usuario> saveUser(@RequestBody Usuario usuario){
+		
+		
+		return ResponseEntity.ok().body(usuarioService.save(usuario));
+		
 		
 	}
 	
